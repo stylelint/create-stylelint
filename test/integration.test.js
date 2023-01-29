9f870f53aa2296658f7c3bd4ab6ddabd8ae7e984
Matthew Wang <matt@matthewwang.me>
2023-01-29 10:23:12 -0800

    applies test suggestions

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line node/no-unpublished-import
 import { describe, expect, it } from 'vitest';
-import { execaSync } from 'execa';
+import { execFileSync } from 'node:child_process';
 
 const inputs = {
 	stylelintConfigExists: 'test/fixtures/stylelint-config-exists',
@@ -11,7 +11,7 @@ function getProjectRoot(context) {
 }
 
 function setup(pathToTest, projectRoot, args = []) {
-	return execaSync('node', [`${projectRoot}create-stylelint.mjs`, ...args], {
+	return execFileSync(`${projectRoot}create-stylelint.mjs`, args, {
 		cwd: `${projectRoot}${pathToTest}`,
 	});
 }
@@ -20,11 +20,8 @@ describe('stylelint-create', () => {
 	it('should not proceed if a Stylelint config already exists in the directory', (context) => {
 		const projectRoot = getProjectRoot(context);
 
-		try {
-			setup(inputs.stylelintConfigExists, projectRoot);
-		} catch (error) {
-			expect(error.failed).toBe(true);
-			expect(error.message).toMatch('config(s) already exist');
-		}
+		expect(() => setup(inputs.stylelintConfigExists, projectRoot)).toThrowError(
+			/config\(s\) already exist/,
+		);
 	});
 });
