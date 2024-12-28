d12d28b8a614d72f3e6acc971cd1e64184830e99
Eugene H. (idkgene) <webeugene@proton.me>
2024-12-28 14:39:51 +0300

    feat: cancel support for prompts

--- src/prompts/install-now.ts
+++ src/prompts/install-now.ts
@@ -1,17 +1,27 @@
+import picocolors from 'picocolors';
 import prompts from 'prompts';
 
 export async function promptInstallDependencies(
 	pkgManager: string,
 	dependencies: string[],
 ): Promise<boolean> {
-	const { installDependencies } = await prompts({
-		type: 'confirm',
-		name: 'installDependencies',
-		message: `The following dependencies will be installed using ${pkgManager}:\n\n  ${dependencies.join(
-			'\n  ',
-		)}\n\nWould you like to proceed with the installation?`,
-		initial: true,
-	});
+	const { installDependencies } = await prompts(
+		{
+			type: 'confirm',
+			name: 'installDependencies',
+			message: `The following dependencies will be installed using ${pkgManager}:\n\n  ${dependencies.join(
+				'\n  ',
+			)}\n\nWould you like to proceed with the installation?`,
+			initial: true,
+		},
+		{
+			onCancel: () => {
+				console.log('\n');
+				console.log(picocolors.yellow('Operation cancelled by user.'));
+				process.exit(1);
+			},
+		},
+	);
 
 	return installDependencies as boolean;
 }
--- src/prompts/package-manager.ts
+++ src/prompts/package-manager.ts
@@ -1,34 +1,44 @@
 import prompts from 'prompts';
 import detectPackageManager from 'preferred-pm';
+import picocolors from 'picocolors';
 
 export type PackageManagerChoice = 'npm' | 'pnpm' | 'yarn' | 'bun';
 
 export async function promptPackageManager(): Promise<PackageManagerChoice> {
 	const detectedPackageManager = await detectPackageManager(process.cwd());
 
-	const { packageManager } = await prompts({
-		type: 'select',
-		name: 'packageManager',
-		message: 'Which package manager do you want to use for installing dependencies?',
-		choices: [
-			{ title: 'npm', value: 'npm' },
-			{ title: 'pnpm', value: 'pnpm' },
-			{ title: 'yarn', value: 'yarn' },
-			{ title: 'bun', value: 'bun' },
-		],
-		initial: (() => {
-			switch (detectedPackageManager?.name) {
-				case 'pnpm':
-					return 1;
-				case 'yarn':
-					return 2;
-				case 'bun':
-					return 3;
-				default:
-					return 0;
-			}
-		})(),
-	});
+	const { packageManager } = await prompts(
+		{
+			type: 'select',
+			name: 'packageManager',
+			message: 'Which package manager do you want to use for installing dependencies?',
+			choices: [
+				{ title: 'npm', value: 'npm' },
+				{ title: 'pnpm', value: 'pnpm' },
+				{ title: 'yarn', value: 'yarn' },
+				{ title: 'bun', value: 'bun' },
+			],
+			initial: (() => {
+				switch (detectedPackageManager?.name) {
+					case 'pnpm':
+						return 1;
+					case 'yarn':
+						return 2;
+					case 'bun':
+						return 3;
+					default:
+						return 0;
+				}
+			})(),
+		},
+		{
+			onCancel: () => {
+				console.log('\n');
+				console.log(picocolors.yellow('Operation cancelled by user.'));
+				process.exit(1);
+			},
+		},
+	);
 
 	return packageManager as PackageManagerChoice;
 }
