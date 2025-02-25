ad7d5f6b1c73c3d76bad45b175b189fea8872611
outslept <webeugene@proton.me>
2025-02-25 13:52:27 +0300

    removing mentions of nvm because nodejs/download mentions them already

--- create-stylelint.mjs
+++ create-stylelint.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /* eslint-disable n/prefer-global/process */
-/* eslint-disable n/no-process-exit */
+ 
 
 const currentVersion = process.versions.node;
 const requiredMajorVersion = parseInt(currentVersion.split('.')[0], 10);
@@ -10,9 +10,7 @@ const minimumMajorVersion = 18;
 if (requiredMajorVersion < minimumMajorVersion) {
 	process.stderr(`Error: Node.js v${currentVersion} is not supported.`);
 	process.stderr(`This tool requires Node.js v${minimumMajorVersion} or higher.`);
-    process.stderr('Please update your Node.js installation:');
-    process.stderr('- Visit: https://nodejs.org/');
-    process.stderr('- Or use a version manager like nvm: https://github.com/nvm-sh/nvm');
+    process.stderr('Please update your Node.js installation by following the instructions at https://nodejs.org/en/download');
 	process.exit(1)
 }
 
