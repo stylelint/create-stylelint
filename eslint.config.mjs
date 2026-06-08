4a0b953ff7ba55beeb6dad0adf2e6c75c8e3319c
Richard Hallows <jeddy3@users.noreply.github.com>
2026-06-08 18:39:43 +0100

    Fix merge

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,4 @@
 // @ts-ignore
 import stylelintConfig from 'eslint-config-stylelint';
 
-export default [
-	...stylelintConfig,
-	{
-		rules: {
-			// TODO: Remove when we drop support for Node.js 20
-			'n/no-unsupported-features/node-builtins': [
-				'error',
-				{
-					ignores: ['util.styleText', 'test.describe', 'import.meta.dirname'],
-				},
-			],
-		},
-	},
-];
+export default [...stylelintConfig];
